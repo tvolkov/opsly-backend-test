6681855ace19f72cbfa2867b6f17fc56cf4bd1d1
Timofei Volkov <timofei.volkov@tesco.com>
2020-11-11 08:49:53 +0000

    global error handler

---
+++ src/main/java/com/opslycloud/errorhandler/ErrorAttributes.java
@@ -0,0 +1,8 @@
+package com.opslycloud.errorhandler;
+
+import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+
+//@Component
+public class ErrorAttributes extends DefaultErrorAttributes {
+}
