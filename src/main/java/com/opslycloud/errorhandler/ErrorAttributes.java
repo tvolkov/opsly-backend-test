1b448141b05432dbcbcc08cf6b0c65e85c2d46ad
Timofei Volkov <timofei.volkov@tesco.com>
2020-11-11 08:50:21 +0000

    remove unused code

--- src/main/java/com/opslycloud/errorhandler/ErrorAttributes.java
+++
@@ -1,8 +0,0 @@
-package com.opslycloud.errorhandler;
-
-import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
-import org.springframework.stereotype.Component;
-
-//@Component
-public class ErrorAttributes extends DefaultErrorAttributes {
-}
